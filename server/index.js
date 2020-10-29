9a872da6f8b76a73c29cfe5926667e4c6f3e8cef
Austin Carly <austinrcarly16@gmail.com>
2020-10-29 14:05:12 -0600

    remove spaces

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const {
 } = process.env;
 
 app.use(express.json());
-app.use( express.static( `${__dirname}/../build` ) );
+app.use(express.static(`${__dirname}/../build`));
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
