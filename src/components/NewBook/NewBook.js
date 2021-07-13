66fc271154daf245a863e1a2b614110d8ad5458e
Austin Carly <austin@brightbridgeweb.com>
2021-07-12 19:54:49 -0600

    Added New Book Forgotten Queen

--- src/components/NewBook/NewBook.js
+++ src/components/NewBook/NewBook.js
@@ -6,7 +6,7 @@ import { withRouter } from "react-router-dom";
 import { RiEditFill } from "react-icons/ri";
 // import NewBookEdit from "./NewBookEdit/NewBookEdit";
 import "../NewBook/NewBook.css";
-import comingSoonImage from "../../Photos/THE STOLEN PRINCESS.png"
+import comingSoonImage from "../../Photos/THE FORGOTTEN QUEEN.jpg";
 
 // -------------------------------------FUNCTIONALITY--------------------------------------
 // -----------------------------------STRUCTURE---------------------------------------------
@@ -18,17 +18,49 @@ function Newbook(props) {
         {/* <div className="editNewBookButton"><RiEditFill/></div> */}
       </div>
       {/* <p className="newBookTitleTempTwo">Now Available!</p> */}
-      <p className="newBookTitleTempTwo">Now Available!</p>
+      <p className="newBookTitleTempTwo">Coming Soon!</p>
       <section className="imageOneBoxContentTemp">
         <img src={comingSoonImage} className="imageOneTemp" alt="Book" />
         <div className="newBookTitle-InfoTemp">
           <hr />
-          <p className="newBookReleaseDateTempTitle">The Stolen Princess</p>
+          <p className="newBookReleaseDateTempTitle">The Forgotten Queen</p>
+          <div className="releaseDateBox">
+            <p>Available</p>
+            <p className="newBookReleaseDate">September 27th 2021</p>
+          </div>
+          <hr />
+          <div className="newBookDescriptionsContainer">
+            <p className="newBookDescription">
+              She's trying to remember. He's trying to pretend.
+            </p>
+            <p className="newBookDescription">
+              Sydria Hasler is a blank slate. After a tragic accident, her
+              images of the past have vanished. All she wants is to get her
+              memory back and return home—wherever home is. But the more she
+              rebuilds her past, the more she wants to fight for a future with
+              King Marx. Too bad it's all a sham.
+            </p>
+            <p className="newBookDescription">
+              King Marx McKane has never cared about anyone or anything,
+              especially being king. When his father forces him to marry a
+              mysterious woman, he starts taking things seriously. The only way
+              out of his arranged marriage is to pretend that he's falling in
+              love with his wife. This charade might end up being the most
+              reckless thing Marx has ever done. Is he willing to sacrifice his
+              life and his kingdom to set Sydria free?
+            </p>
+            <p className="newBookDescription">
+              Can their fake relationship lead Sydria and Marx to their genuine
+              selves, or will deception cost them their freedom?
+            </p>
+          </div>
           <div className="buyButtonContainerTemp">
             <hr />
             <li className="buyLinkButtonBoxTemp">
-              <a href={`https://www.amazon.com/Stolen-Princess-YA-Dystopian-Romance/dp/B092PGCY7B/ref=sr_1_2?dchild=1&keywords=stolen+princess&qid=1619485031&sr=8-2`}>
-                PURCHASE
+              <a
+                href={`https://www.amazon.com/gp/product/B098ZN9DNS/ref=dbs_a_def_rwt_hsch_vapi_tkin_p1_i1`}
+              >
+                PRE-ORDER
               </a>
             </li>
           </div>
